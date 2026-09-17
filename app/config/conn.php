1c315c5780f9b1efc87ffce72b81862b9d487798
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-09-17 18:10:43 -0300

    add: conexao com banco de dados

--- app/config/conn.php
+++ app/config/conn.php
@@ -1,18 +1,13 @@
 <?php
 
-$host = 'locallhost';
-
-$username = '';
+$host = 'localhost';
+$username = 'root';
 $password = '';
-$dbname = 'parnaioca';
+$dbname = 'parnaoica';
 $dns = "mysql:host=$host;dbname=$dbname";
-
-
-
 
 try {
     $conn = new PDO($dns, $username, $password);
-    echo 'Conexao bem sucedida';
 } catch (PDOException $e) {
     echo 'erro ao conectar' . $e->getMessage();
 }
