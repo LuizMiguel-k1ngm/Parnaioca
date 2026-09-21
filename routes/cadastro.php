96b53d0e0ba5d0cc55d9404cef51ff64de1b80be
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-09-21 14:54:07 -0300

    add: rotas e novas arquivos

--- routes/cadastro.php
+++ routes/cadastro.php
@@ -2,4 +2,8 @@
 
 return [
     'GET /cadastro' => __DIR__ . '/../app/Views/cadastro/index.php',
+    'GET /cadastro/hospede' => __DIR__ . '/../app/Views/cadastro/hospede/index.php',
+    'GET /cadastro/funcionario' => __DIR__ . '/../app/Views/cadastro/funcionario/index.php',
+    'GET /cadastro/itens' => __DIR__ . '/../app/Views/cadastro/itens/index.php'
+
 ];
