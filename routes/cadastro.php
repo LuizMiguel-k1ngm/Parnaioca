bd7b05f1902052b1c5f0be3deae1c830423d158b
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-09-21 21:27:19 -0300

    add: routes

--- routes/cadastro.php
+++ routes/cadastro.php
@@ -4,6 +4,8 @@ return [
     'GET /cadastro' => __DIR__ . '/../app/Views/cadastro/index.php',
     'GET /cadastro/hospede' => __DIR__ . '/../app/Views/cadastro/hospede/index.php',
     'GET /cadastro/funcionario' => __DIR__ . '/../app/Views/cadastro/funcionario/index.php',
-    'GET /cadastro/itens' => __DIR__ . '/../app/Views/cadastro/itens/index.php'
+    'GET /cadastro/itens' => __DIR__ . '/../app/Views/cadastro/itens/index.php',
+    'GET /cadastro/quarto' => __DIR__ . '/../app/Views/cadastro/quarto/index.php',
+    'GET /cadastro/frigobar' => __DIR__ . '/../app/Views/cadastro/frigobar/index.php'
 
 ];
